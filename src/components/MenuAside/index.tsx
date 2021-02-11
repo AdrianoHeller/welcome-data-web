a7b8e5510695a3d0bf33c19d3e98ab8df899da68
Adriano Heller Mylla <adrianohellermylla@bb.com.br>
2021-02-11 12:00:56 -0300

    updating components

--- src/components/MenuAside/index.tsx
+++ src/components/MenuAside/index.tsx
@@ -1,22 +1,94 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { Link } from 'react-router-dom';
+import { 
+    MdAccountCircle,
+    MdAirplanemodeActive,
+    MdAttachMoney,
+    MdMemory,
+    MdInsertChart,
+    MdHighlight,
+    MdPhoneIphone,
+    MdDashboard
+    } from 'react-icons/md';
 import { 
     Container,
     BadgeContainer,
     BadgeData,
-    BadgeImage,
-    BadgeText } from './style';
+    BadgeWrapper,
+    BadgeIcon,
+    BadgeHr,
+    BadgeAvatar,
+    BadgeText,
+    OptionContainer,
+    OptionList,
+    Option } from './style';
 
-import UserAvatar from '../../assets/logo.svg';
+import WelcomeIcon from '../../assets/logo.svg';
+import UserAvatar from '../../assets/mestre.png';
 
 const MenuAside: React.FC = () => {
+    
+    const [currentHour,setCurrentHour] = useState();
+
+    const generateDate: Function = (): string => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const getTime = `${date.getHours()}:${date.getMinutes()}`
+        return `${day}/${month}/${year}, às ${getTime}`;
+    };
+
     return(
         <Container>
-            <BadgeContainer>
-                <BadgeData>
-                    <BadgeImage src={UserAvatar}/>
-                    <BadgeText />
-                </BadgeData>
-            </BadgeContainer>
+                    <BadgeContainer>
+                        <BadgeWrapper>
+                            <BadgeIcon src={WelcomeIcon}/>
+                        </BadgeWrapper>
+                        <BadgeWrapper>
+                            <BadgeAvatar src={UserAvatar}/>
+                        </BadgeWrapper>
+                        <BadgeText>
+                            <BadgeData>Adriano Heller Mylla</BadgeData>
+                            <BadgeData>CTO</BadgeData>
+                        </BadgeText>
+                        <BadgeText>
+                            <BadgeData>Último acesso em :<br/>{generateDate()}</BadgeData>
+                        <BadgeHr/>
+                        </BadgeText>
+                    </BadgeContainer>
+                    <OptionContainer>
+                        <OptionList>
+                            <Option>
+                                <MdAccountCircle/>
+                                <Link to={'/DashPeople'}>Clientes</Link>
+                            </Option>
+                            <Option>
+                                <MdAirplanemodeActive/>
+                                <Link to={'/DashTrips'}>Viagens</Link>
+                            </Option>
+                            <Option>
+                                <MdAttachMoney/>
+                                <Link to={'/DashMoney'}>Financeiros</Link>
+                            </Option>
+                            <Option>
+                                <MdDashboard/>
+                                <Link to={'/DashReports'}>Relatórios</Link>
+                            </Option>
+                            <Option>
+                                <MdHighlight/>
+                                <Link to={'/DashEvents'}>Ocorrências</Link>
+                            </Option>
+                            <Option>
+                                <MdPhoneIphone/>
+                                <Link to={'/DashApis'}>Integrações</Link>
+                            </Option>
+                            <Option>
+                                <MdMemory/>
+                                <Link to={'/DashWeex'}>Weex</Link>
+                            </Option>
+                        </OptionList>
+                    </OptionContainer>           
         </Container>
     )
 };
--- src/components/MenuAside/style.ts
+++ src/components/MenuAside/style.ts
@@ -3,16 +3,34 @@ import styled from 'styled-components';
 export const Container = styled.div`
     grid-area: MenuAside;
     display: flex;
+    flex-direction:column;
     background-color:${props => props.theme.colors.mainHeader};
+    height:100%;
+    font-family:'Rajdhani';
+    
+`;
+
+export const BadgeWrapper = styled.div`
+    display: flex;
+    flex-direction:column;
 `;
 
 export const BadgeContainer = styled.div`
     display: flex;
+    flex-direction:column;
+    width:100%;    
 `;
 
 
-export const BadgeImage = styled.img`
-    width: 100%;
+export const BadgeIcon = styled.img`
+    width: 50%;
+    margin:0 auto;
+`;
+
+export const BadgeAvatar = styled.img`
+    width: 50%;
+    margin:0 auto;
+    border-radius:15%;
 `;
 
 export const BadgeText = styled.div`
@@ -22,6 +40,38 @@ export const BadgeText = styled.div`
     justify-content: center;
 `;
 
+export const BadgeHr = styled.hr`
+    border: 2px solid ${props => props.theme.colors.textDark};
+    width: 100%;
+`;
+
 export const BadgeData = styled.p`
+    margin: 0;
+    padding: 0;
     color: ${props => props.theme.colors.textMedium};
+`;
+
+export const OptionContainer = styled.div`
+    height: 60%;
+    display: flex;
+    flex-direction:column;
+    width: 100%;
+    justify-content:center;
+    align-items:center;
+`;
+
+export const OptionList = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`;
+
+export const Option = styled.li`
+    > svg{
+        color:${props => props.theme.colors.textMedium};
+    };
+    > a{
+        text-decoration: none;
+        color:${props => props.theme.colors.textMedium};
+    };
 `;
